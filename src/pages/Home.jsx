07ca044bab3e28e7f9d5c3b454759bc2bd503f9f
jaime chico loayza <jrchldev@gmail.com>
2024-07-04 14:34:42 -0500

    actualizado cv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,32 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from 'react';
 import Hero from './Hero/Hero';
 import NavBar from '../components/NavBar/NavBar';
 import Tecnology from './Tecnology/Tecnology';
 import Myproject from './Myprojects/Myproject';
-import Contact from "./Contact/Contact";
+import Contact from './Contact/Contact';
 import AOS from 'aos';
-import Aboutme from "./Aboutme/Aboutme";
-
+import Aboutme from './Aboutme/Aboutme';
 
 import 'aos/dist/aos.css';
 
 const Home = () => {
-
+  
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
 
-  
+
   return (
-    <div className="absolute top-0 z-[10] h-auto w-full ">
-        <NavBar></NavBar>
+    <div className="absolute top-0 z-[10] w-full h-auto">
+      <NavBar></NavBar>
         <Hero></Hero>
         <Aboutme></Aboutme>
         <Myproject></Myproject>
         <Tecnology></Tecnology>
 
         <Contact></Contact>
-        <div className="bg-hero-primary bg absolute inset-x-0 inset-y-0 -z-20 bg-fixed bg-[length:90px] grayscale"></div>
-        <div className="bg-hero-secondary bg absolute inset-x-0 inset-y-0 -z-20  bg-fixed  bg-cover grayscale opacity-10" ></div>
+      <div className="bg-hero-primary bg absolute inset-x-0 inset-y-0 -z-20 bg-fixed bg-[length:90px] grayscale"></div>
+      <div className="bg-hero-secondary bg absolute inset-x-0 inset-y-0 -z-20  bg-fixed  bg-cover grayscale opacity-10"></div>
     </div>
   );
 };
